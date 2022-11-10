99e080eb9dd82c6a1f0354c6e6f52595aeba9776
Shun Git <kwokshungit@gmail.com>
2022-11-10 22:43:12 +0800

    replace readme

--- .github/NEW_README.md
+++ .github/NEW_README.md
@@ -1,7 +1,5 @@
 # gh-template-tests
-GitHub template repository experiments
 
 [![Open in Codeflow](https://developer.stackblitz.com/img/open_in_codeflow_small.svg)](https://pr.new/randName/gh-template-tests)
 
-## quick start guide
-how to start quickly
+lets get down to business
--- .github/update_links.sh
+++ .github/update_links.sh
@@ -5,9 +5,11 @@ echo "now at $1"
 original_name="randName/gh-template-tests"
 
 for filename in $(git ls-files); do
-	sed -i "s/$original_name/$1/g" $filename
+	sed -i "s|$original_name|$1|g" $filename
 	echo "updated $filename"
 done
+
+mv .github/NEW_README.md README.md
 
 rm -rf .github
 rm -rf docs
